0a7f6548aa8dbf81f55ba505e267817918fd0fb1
Emanuele Fia <emanuele.fia@gmail.com>
2017-05-21 22:02:18 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/sh -e
 
-sudo apt-get install build-essentials imagemagick git -y
+apt-get update
+apt-get install build-essential imagemagick git -y
 
 mkdir -p /opt/repo
 cd /opt/repo
 git clone https://github.com/name29/simpleFileTransfer.git
 cd simpleFileTransfer
-gcc server.cpp -Wall -o sfp-server
+gcc server.cpp -Wall -o sft-server
 mkdir -p /usr/local/bin
-cp sfp-server /usr/local/bin
+cp sft-server /usr/local/bin
 
 
 
